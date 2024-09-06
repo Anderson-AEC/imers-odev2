528cc315188cf298b1076f577fba4136971e88e4
Anderson-AEC <andersondasilvagomes1@gmail.com>
2024-09-05 22:14:50 -0300

    mudancarepentina

---
+++ dados.js
@@ -0,0 +1,52 @@
+let dados = [
+    {
+        titulo: "Engenheiro de Machine Learning",
+        descricao: "Desenvolve algoritmos de aprendizado de máquina para criar modelos preditivos.",
+        exemplo: "Utiliza IA para criar e treinar modelos que aprendem padrões em dados, como reconhecimento de imagens, processamento de linguagem natural e análise de séries temporais."
+    },
+    {
+        titulo: "Professor",
+        descricao: "Transmite conhecimento e habilidades para aluno",
+        exemplo: "Criação de materiais didáticos personalizados, plataformas de ensino online inteligentes, avaliação automatizada de trabalhos, tutoria virtual e análise de dados para identificar dificuldades dos alunos."
+    },
+    {
+        titulo: "Médico",
+        descricao: "Diagnostica, trata e previne doenças.",
+        exemplo: "Análise de imagens médicas para detecção precoce de doenças, desenvolvimento de novos medicamentos, criação de assistentes virtuais para agendamento de consultas e fornecimento de informações médicas, e personalização de tratamentos."
+    },
+    {
+        titulo: "Arquiteto",
+        descricao: "Projeta e constrói edifícios e espaços.",
+        exemplo: "Criação de modelos 3D mais realistas e detalhados, simulação de projetos para otimizar recursos, análise de dados para prever o desempenho energético de edifícios e geração de designs inovadores."
+    },
+    {
+        titulo: "Designer",
+        descricao: "Cria projetos visuais para diversas mídias",
+        exemplo: "Geração de ideias criativas, criação de logotipos e imagens, personalização de designs, otimização de layouts e análise de dados para entender as preferências dos consumidores."
+    },
+    {
+        titulo: "Eletricistar",
+        descricao: "Instala, mantém e repara sistemas elétricos.",
+        exemplo: "Manutenção preditiva de equipamentos, otimização de redes elétricas, análise de dados para identificar falhas e otimizar o consumo de energia."
+    },
+    {
+        titulo: "Motorista",
+        descricao: "Conduz veículos para transportar pessoas ou cargas.",
+        exemplo: "Veículos autônomos, sistemas de assistência ao motorista (como frenagem automática e piloto automático), otimização de rotas e gestão de frotas."
+    },
+    {
+        titulo: "Veterinário",
+        descricao: "Cuida da saúde de animais.",
+        exemplo: "Análise de imagens médicas para diagnosticar doenças, desenvolvimento de tratamentos personalizados, criação de bancos de dados genéticos de animais e monitoramento remoto da saúde de animais de estimação."
+    },
+    {
+        titulo: "Programador",
+        descricao: "Desenha, escreve, testa e mantém o código de software.",
+        exemplo: "Automação da codificação, geração de código, depuração de programas, testes automatizados e criação de ferramentas de desenvolvimento baseadas em IA."
+    },
+    {
+        titulo: "Cientista de dados",
+        descricao: "Coleta, limpa e analisa grandes volume de dados.",
+        exemplo: "Emprega tecnicas de I.A. para acelerar a analise destes dados, assim e possivel explorar e tomar decisões com mais facilidade."
+    },
+];
